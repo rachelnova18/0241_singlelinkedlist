45bd671d6fc5b54a3acbfb9bffbf2d8f397c9210
rachelnova18 <rachelnv26@gmail.com>
2025-05-06 12:25:48 +0700

    Menambahkan metode delNode untuk menghapus data berdasarkan NIM

--- singly_lingkedlist.cpp
+++ singly_lingkedlist.cpp
@@ -74,4 +74,22 @@ public:
 
         return (*current != NULL);
     }
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!search(nim, &previous, &current))
+        {
+            cout << "\nNoMhs tidak ditemukan\n";
+            return false;
+
+            if (current == START)
+                START = START->next;
+            else
+                previous->next = current->next;
+
+            delete current;
+            return true;
+        }
+    }
+
     
